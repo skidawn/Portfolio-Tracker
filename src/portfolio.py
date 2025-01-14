9797183f89b302e19e5ae8d91492d33762f2832a
Dante <Dominic.Ardavicius.2023@live.rhul.ac.uk>
2025-01-14 00:07:32 +0000

    percentage change method

--- src/portfolio.py
+++ src/portfolio.py
@@ -3,6 +3,23 @@ import pandas
 class Portfolio:
     """Interface for a general portfolio."""
 
+    @staticmethod
+    def percentage_change(old : float, new : float) -> float:
+        """
+        Returns a percentage of the increase of decrease from the old value to the new value.
+
+        Attempting to divide by zero will just return zero.
+
+        Args:
+            old (float): The old value.
+            new (float): The final value.
+
+        Returns:
+            float: The percentage of change or movement from the difference.
+        """
+        if old == 0: return 0 # ZeroDivisonError
+        return ( (new - old) / old ) * 100
+
     class Cash:
         """Interface for the cash and values in a Portfolio."""
         free = 0 # Free cash
